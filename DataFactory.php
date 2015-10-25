82ee3089994b9857c10ba7d0d47f6df376ed8c90
Scott Weldon <scott@scott-weldon.com>
2015-10-24 19:21:56 -0700

    Add DataFactory singleton for storing data

---
+++ DataFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+final class DataFactory {
+  public static function make() {
+    static $inst = null;
+
+    if ($inst == null)
+      $inst = new self;
+
+    return $inst;
+  }
+
+  private function __construct() {
+    // Thou shalt not construct this singleton!
+  }
+
+  private function __clone() {
+    // Thou shalt not clone this singleton!
+  }
+}
+
+?>
