b0c8e19916b71017d83234a8b525106b57556b92
Scott Weldon <scott@scott-weldon.com>
2015-10-24 19:25:32 -0700

    Add loop to init data per config file

--- DataSelector.php
+++ DataSelector.php
@@ -9,6 +9,18 @@ class DataSelector extends Extension {
   ];
 
   public function beforeSuite(TestEvent $e) {
+    $db = new DatabaseSelector;
+
+    foreach ($this->config as $group => $value) {
+      $data = DataFactory::make();
+      $data->$group = new stdClass;
+
+      $result = $db->query($value['table'], $value['fields'], $value['joins'],
+                           $value['conditions']);
+      foreach ($result as $field => $value) {
+        $data->$group->$field = $value;
+      }
+    }
   }
 }
 
