cbf4743d48e9d04dd955a90929cd418971de9a27
Scott Weldon <scott@scott-weldon.com>
2015-11-28 11:34:58 -0800

    Add init code for AspectMock

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 require_once "src/Alpha1_501st/CodeceptionDataSelector/SqlBuilder.php";
+
+include __DIR__.'/../../vendor/autoload.php'; // composer autoload
+
+$kernel = \AspectMock\Kernel::getInstance();
+$kernel->init([
+    'debug' => true,
+    'includePaths' => [__DIR__.'/../src']
+]);
