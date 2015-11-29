54b4e778d04910c1e4461d51f63f3acb92615269
Scott Weldon <scott@scott-weldon.com>
2015-11-28 21:19:01 -0800

    Add test for database selector (WIP)

---
+++ tests/unit/DatabaseSelectorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Alpha1_501st\CodeceptionDataSelector\DatabaseSelector;
+use AspectMock\Test as test;
+
+class DatabaseSelectorTest extends \Codeception\TestCase\Test {
+  /**
+   * @var \UnitTester
+   */
+  protected $tester;
+
+  protected function _before() {
+  }
+
+  protected function _after() {
+  }
+
+  public function testQuery() {
+    $builderMock = test::double('Alpha1_501st\CodeceptionDataSelector\SqlBuilder',
+                                ['generateJoinClause'=>"",
+                                 'generateWhereClause'=>""]);
+    //$pdoMock = test::double('PDO', ['prepare'=>null]);
+    //$pdoStatementMock = test::double('PDOStatement', ['execute'=>null, 'fetch'=>null]);
+
+    // FIXME mock PDO, etc so that MySQL doesn't need to be installed.
+    $db = new DatabaseSelector("mysql:host=localhost;dbname=testdb", "root", "");
+    $ret = $db->query('comments', 'content', [], []);
+
+    $builderMock->verifyInvoked('generateJoinClause');
+  }
+}
+
+?>
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 require_once "src/Alpha1_501st/CodeceptionDataSelector/SqlBuilder.php";
+require_once "src/Alpha1_501st/CodeceptionDataSelector/DatabaseSelector.php";
 
 include __DIR__.'/../../vendor/autoload.php'; // composer autoload
 
