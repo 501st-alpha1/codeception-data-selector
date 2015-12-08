698df7ee6183e79b99fffb384da217c3202911fa
Scott Weldon <scott@scott-weldon.com>
2015-12-08 12:13:28 -0800

    Move AspectMock init to correct bootstrap file

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -4,9 +4,3 @@ require_once "src/Alpha1_501st/CodeceptionDataSelector/SqlBuilder.php";
 require_once "src/Alpha1_501st/CodeceptionDataSelector/DatabaseSelector.php";
 
 include __DIR__.'/../../vendor/autoload.php'; // composer autoload
-
-$kernel = \AspectMock\Kernel::getInstance();
-$kernel->init([
-    'debug' => true,
-    'includePaths' => [__DIR__.'/../../src']
-]);
